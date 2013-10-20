8e86f52397c1c29ebb0ed13c4f9a7bca30e9d9d4
homoluden <asm@metaphysics-it.net>
2013-10-20 13:12:01 +0400

    Will and Representation made singleton :)

--- FukamiDemo/WorldControllers/Representation.cs
+++ FukamiDemo/WorldControllers/Representation.cs
@@ -8,5 +8,30 @@ namespace WorldControllers
 {
     public class Representation
     {
+        
+    #region Singleton
+        private static volatile Representation _instance;
+        private static object syncRoot = new Object();
+
+        private Representation() { }
+
+        public static Representation Instance
+        {
+            get 
+            {
+                if (_instance == null) 
+                {
+                lock (syncRoot) 
+                {
+                    if (_instance == null)
+                        _instance = new Representation();
+                }
+                }
+
+                return _instance;
+            }
+        }
+	#endregion
+
     }
 }
--- FukamiDemo/WorldControllers/Will.cs
+++ FukamiDemo/WorldControllers/Will.cs
@@ -8,5 +8,31 @@ namespace WorldControllers
 {
     public class Will
     {
+
+
+    #region Singleton
+        private static volatile Will _instance;
+        private static object syncRoot = new Object();
+
+        private Will() { }
+
+        public static Will Instance
+        {
+            get 
+            {
+                if (_instance == null) 
+                {
+                lock (syncRoot) 
+                {
+                    if (_instance == null)
+                        _instance = new Will();
+                }
+                }
+
+                return _instance;
+            }
+        }
+	#endregion
+        
     }
 }
