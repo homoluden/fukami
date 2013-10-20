7e6567215e95a6dc7f19c4cf29069bc0aaa0b338
homoluden <asm@metaphysics-it.net>
2013-10-20 22:18:02 +0400

    AddJoint implemented in Will

--- FukamiDemo/WorldControllers/Will.cs
+++ FukamiDemo/WorldControllers/Will.cs
@@ -69,9 +69,9 @@ namespace WorldControllers
             return actualAdded;
         }
 
-        public void AddJoint(HingeJoint joint)
+        public void AddJoint(Joint joint)
         {
-            throw new NotImplementedException();
+            _engine.AddJoint(joint);
         }
 
         /// <summary>
