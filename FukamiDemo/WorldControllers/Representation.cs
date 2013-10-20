150dcbffa0287160ee5323588c1448de94ce2eac
homoluden <asm@metaphysics-it.net>
2013-10-20 17:39:52 +0400

    Added RepresentationHelper with Unsafe Chain helper

--- FukamiDemo/WorldControllers/Representation.cs
+++ FukamiDemo/WorldControllers/Representation.cs
@@ -1,4 +1,5 @@
 ﻿using Drawables;
+using Physics2DDotNet;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
---
+++ FukamiDemo/WorldControllers/RepresentationHelper.cs
@@ -0,0 +1,27 @@
+﻿using Drawables;
+using Physics2DDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldControllers
+{
+    public static class RepresentationHelper
+    {
+        /// <summary>
+        /// Adds all elements of the chain as Drawables into Representation
+        /// </summary>
+        /// <param name="chainMembers">The elements of the chain excluding the begining and ending fixed bodies</param>
+        /// <remarks>Make it sure that all items in List has "Guid" and "Drawable" values in their "Body.Tags" dictionary</remarks>
+        public static void AddChainDrawablesUnsafe(IList<Body> chainMembers)
+        {
+            foreach (var body in chainMembers)
+            {
+                Representation.Instance.AddOrReplaceDrawable((Guid)body.Tags["Guid"], (ColoredPolygonDrawable)body.Tags["Drawable"]);
+            }
+        }
+
+    }
+}
