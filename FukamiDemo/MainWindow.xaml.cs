f45421237fa0d5258d8dff81b86a072323d5e127
homoluden <asm@metaphysics-it.net>
2013-10-21 00:23:00 +0400

    UI - ViewModel connection

--- FukamiDemo/MainWindow.xaml.cs
+++ FukamiDemo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FukamiDemo.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace FukamiDemo
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new ChainDemoViewModel();
         }
     }
 }
