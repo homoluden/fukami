5605baebf4d0eddf68a9e3c860a746475f97c70e
homoluden <asm@metaphysics-it.net>
2013-11-23 19:23:53 +0400

    HingeSmoothPos animation script added

---
+++ Fukami.Unity3D/Assets/Scripts/HingeSmoothPos.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HingeSmoothPos : MonoBehaviour {
+
+	public float AnchorX = 0.0f;
+	public float AnchorY = 0.0f;
+	public float Duration = 3.0f;
+
+	private HingeJoint2D _hinge;
+	private float _age = Time.fixedDeltaTime;
+
+	// Use this for initialization
+	void Start () {
+		_hinge = gameObject.GetComponent<HingeJoint2D>();
+		if (_hinge == null) {
+			enabled = false;
+		}
+		else {
+			AnchorX = _hinge.connectedAnchor.x;
+			AnchorY = _hinge.connectedAnchor.y;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_age >= Duration) {
+			enabled = false;
+			_age = 0.0f;
+			_hinge.connectedAnchor = new Vector2(AnchorX, AnchorY);
+			return;
+		}
+
+		_age += Time.deltaTime;
+
+		var t = _age / Duration;
+
+		var anchor = new Vector2(Mathf.Lerp(0.0f, AnchorX, t), Mathf.Lerp(0.0f, AnchorY, t));
+
+		_hinge.connectedAnchor = anchor;
+	}
+}
