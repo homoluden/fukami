fee64bb0f624d780585321bb4fd2f8cd2dd39b69
Alexey Matveev <alexey_matveev@epam.com>
2014-01-02 00:47:57 +0400

    Added Voronoi Generator library to Unity3d

---
+++ Fukami.Unity3D/Assets/Scripts/VoronoiTest.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using SteeleSky.Voronoi;
+
+public class VoronoiTest : MonoBehaviour
+{
+    #region Fields
+
+    VoronoiGraph _graph;
+
+    #endregion
+
+    #region Properties
+
+    public Vector2[] Vertices;
+
+    #endregion
+
+    // Use this for initialization
+	void Start () {
+        Vertices = new Vector2[] { 
+        new Vector2(Random.Range(-50f, 50f), Random.Range(-50f, 50f)),
+        new Vector2(Random.Range(-50f, 50f), Random.Range(-50f, 50f)),
+        new Vector2(Random.Range(-50f, 50f), Random.Range(-50f, 50f)),
+        new Vector2(Random.Range(-50f, 50f), Random.Range(-50f, 50f)),
+        new Vector2(Random.Range(-50f, 50f), Random.Range(-50f, 50f)),
+        new Vector2(Random.Range(-50f, 50f), Random.Range(-50f, 50f)),
+        new Vector2(Random.Range(-50f, 50f), Random.Range(-50f, 50f)),
+        new Vector2(Random.Range(-50f, 50f), Random.Range(-50f, 50f)),
+        new Vector2(Random.Range(-50f, 50f), Random.Range(-50f, 50f)),
+        new Vector2(Random.Range(-50f, 50f), Random.Range(-50f, 50f))
+    };
+        _graph = Fortune.GenerateGraph(Vertices);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
