da1bc53ee6b73875c7b72dcd1079cf69135ab225
homoluden <asm@metaphysics-it.net>
2014-02-09 22:10:48 +0400

    VorTile prefab created
    This prefab have VorTile script, left and right triggers to create
    neighbor VorTile

--- Fukami.Unity3D/Assets/Scripts/VorTile.cs
+++ Fukami.Unity3D/Assets/Scripts/VorTile.cs
@@ -13,21 +13,44 @@ public class VorTile : MonoBehaviour
 
 	public int Seed;
 	public Material[] MeshMaterials;
-	public Vector2 TileSize;
+    public GameObject LeftTileTrigger;
+    public GameObject RightTileTrigger;
 
 	// Use this for initialization
 	void Start ()
 	{
+
 		Random.seed = Seed;
 
 		_cells = VoronoiHelper.Instance.GenerateTileCells (Random.Range (VoronoiHelper.Instance.InternalSitesCountRange [0],
                                                                 		 VoronoiHelper.Instance.InternalSitesCountRange [1]));
 
 		for (int i = 0; i < _cells.Length; i++) {
-			var newCell = VoronoiHelper.Instance.CreateCellObject(_cells[i], i, gameObject.transform, MeshMaterials[Random.Range(0, MeshMaterials.Length)]);
+			var newCell = VoronoiHelper.Instance.CreateCellObject(_cells[i], 
+			                                                      i, 
+			                                                      gameObject.transform, 
+			                                                      MeshMaterials[Random.Range(0, MeshMaterials.Length)]);
 
 			_cellObjects.Add(newCell);
 		}
+
+        var tileSize = VoronoiHelper.Instance.TilesSize;
+
+        if (LeftTileTrigger != null)
+        {
+            var leftCollider = LeftTileTrigger.GetComponent<BoxCollider2D>();
+            LeftTileTrigger.transform.localPosition = new Vector2(tileSize.x * 0.5f, 0f);
+            leftCollider.size = new Vector2(tileSize.x * 0.25f, tileSize.y * 1.5f);
+            leftCollider.isTrigger = true;
+        }
+
+        if (RightTileTrigger != null)
+        {
+            var rightCollider = RightTileTrigger.GetComponent<BoxCollider2D>();
+            RightTileTrigger.transform.localPosition = new Vector2(-tileSize.x * 0.5f, 0f);
+            rightCollider.size = new Vector2(tileSize.x * 0.25f, tileSize.y * 1.5f);
+            rightCollider.isTrigger = true;
+        }
 	}
 
 	// Update is called once per frame
