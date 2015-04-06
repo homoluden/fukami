6b84c627a6941a94344e1df8627cab04ca0b4f15
Alexey Matveev <as.luden@gmail.com>
2015-04-06 20:52:52 +0300

    Moved Polygon stuff

---
+++ FukamiDemo/Drawables/ColoredPolygon.cs
@@ -0,0 +1,18 @@
+﻿using FarseerPhysics.Common;
+using Microsoft.Xna.Framework;
+
+namespace Drawables
+{
+    public class ColoredPolygon : Polygon
+    {
+        public Vector4 Fill { get; private set; }
+        public Vector4 Border { get; private set; }
+
+        public ColoredPolygon(Vertices vertices, Vector4 fill, Vector4 border)
+            :base(vertices)
+        {
+            Fill = fill;
+            Border = border;
+        }
+    }
+}
---
+++ FukamiDemo/Drawables/Polygon.cs
@@ -0,0 +1,14 @@
+﻿using FarseerPhysics.Common;
+
+namespace Drawables
+{
+	public class Polygon
+    {
+        public Vertices Vertices { get; private set; }
+
+        public Polygon(Vertices vertices)
+        {
+            Vertices = vertices;
+        }
+    }
+}
