6e5c9a95250309aa9661e25a928af3a6c37e6a3e
Alexey S. Matveev <alexey.matveev@opensoftdev.ru>
2018-07-05 19:22:23 +0400

    Add draft drawer and right sidebar.

--- web/src/components/home/HomeScreen.vue
+++ web/src/components/home/HomeScreen.vue
@@ -1,13 +1,84 @@
 <template>
-  <div>
-    <div class="viewport">
+  <div class="page-container md-layout-column">
+    <md-toolbar class="md-primary">
+      <md-button class="md-icon-button" @click="showNavigation = true">
+        <md-icon>menu</md-icon>
+      </md-button>
+      <span class="md-title">Fukami Game Resources</span>
+
+      <div class="md-toolbar-section-end">
+        <md-button @click="showSidepanel = true">Favorites</md-button>
+      </div>
+    </md-toolbar>
+    
+    <md-drawer :md-active.sync="showNavigation">
+      <md-toolbar class="md-transparent" md-elevation="0">
+        <span class="md-title">My App name</span>
+      </md-toolbar>
+
+      <md-list>
+        <md-list-item>
+          <md-icon>move_to_inbox</md-icon>
+          <span class="md-list-item-text">Inbox</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>send</md-icon>
+          <span class="md-list-item-text">Sent Mail</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>delete</md-icon>
+          <span class="md-list-item-text">Trash</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>error</md-icon>
+          <span class="md-list-item-text">Spam</span>
+        </md-list-item>
+      </md-list>
+    </md-drawer>
+
+    <md-drawer class="md-right" :md-active.sync="showSidepanel">
+      <md-toolbar class="md-transparent" md-elevation="0">
+        <span class="md-title">Favorites</span>
+      </md-toolbar>
+
+      <md-list>
+        <md-list-item>
+          <span class="md-list-item-text">Abbey Christansen</span>
+
+          <md-button class="md-icon-button md-list-action">
+            <md-icon class="md-primary">chat_bubble</md-icon>
+          </md-button>
+        </md-list-item>
+
+        <md-list-item>
+          <span class="md-list-item-text">Alex Nelson</span>
+
+          <md-button class="md-icon-button md-list-action">
+            <md-icon class="md-primary">chat_bubble</md-icon>
+          </md-button>
+        </md-list-item>
+
+        <md-list-item>
+          <span class="md-list-item-text">Mary Johnson</span>
+
+          <md-button class="md-icon-button md-list-action">
+            <md-icon>chat_bubble</md-icon>
+          </md-button>
+        </md-list-item>
+      </md-list>
+    </md-drawer>
+
+    <md-content class="main-content">
       <md-bottom-bar class="md-alignment-top-center"
         md-type="shift" :md-theme="'bottom-bar-' + theme">
         <md-bottom-bar-item md-label="Home" md-icon="home" @click="theme = 'teal'"></md-bottom-bar-item>
         <md-bottom-bar-item md-label="Pages" md-icon="pages" @click="theme = 'orange'"></md-bottom-bar-item>
         <md-bottom-bar-item md-label="Favorites" md-icon="favorite" @click="theme = 'red'"></md-bottom-bar-item>
       </md-bottom-bar>
-    </div>
+    </md-content>
   </div>
 </template>
 
@@ -17,6 +88,8 @@ export default {
   data() {
     return {
       theme: 'teal',
+      showNavigation: false,
+      showSidepanel: false,
     };
   },
 };
@@ -26,10 +99,30 @@ export default {
 @import "~vue-material/dist/theme/engine";
 
 $active-border-color: md-get-palette-color(bluegrey, 500);
+.page-container {
+  min-height: 100vh;
+  overflow: hidden;
+  position: relative;
+  border: 1px solid rgba(#000, .12);
+}
 
-.viewport {
-  width: 100vw;
-  height: 100vh;
+  // Demo purposes only
+.md-drawer {
+  width: 230px;
+  max-width: calc(100vw - 125px);
+}
+
+.md-content {
+  padding: 16px;
+}
+
+.main-content {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 0;
   display: inline-flex;
   align-items: flex-end;
   overflow: hidden;
