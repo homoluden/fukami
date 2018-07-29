df19cf9b479d580d2348f6fd41328ead3198e823
Alexey S. Matveev <as.luden@gmail.com>
2018-07-29 19:25:36 +0400

    Install Sweet Alert.

--- web/src/main.js
+++ web/src/main.js
@@ -6,12 +6,14 @@ import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default-dark.css';
 
 import Vue from 'vue';
+import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueMaterial);
+Vue.use(VueSweetalert2);
 
 /* eslint-disable no-new */
 new Vue({
